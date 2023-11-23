da632ec954ce618db9faac1a5b60c6c3aabe6efb
Luis-Golac <luis.golac@utec.edu.pe>
2023-11-23 17:39:41 -0500

    pipip

--- backend/ia_api/__init__.py
+++ backend/ia_api/__init__.py
@@ -122,13 +122,13 @@ def voice_recomendations():
             os.remove("../uploads/response.mp3")
 
         # ! Cambiar por la voz de chronos
-        settings = Settings.get_by_user_id(current_user["id"])
-        chronos.change_voice(settings.voice)
+        # settings = Settings.get_by_user_id(current_user["id"])
+        # chronos.change_voice(settings.voice)
 
         # ! Si quieren probar sin azure tts
-        # chronos.make_response_speech_without_azure(response)
+        chronos.make_response_speech_without_azure(response)
 
-        chronos.make_response_speech(response)
+        # chronos.make_response_speech(response)
         confirmation = chronos.parse_response(response)
         print(confirmation)
 
